fc31123792928ba2d43e09680db09423ce4639e2
KhanhNhat242 <86726746+KhanhNhat242@users.noreply.github.com>
2023-08-25 16:30:18 +0700

    xong header footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,6 +1,53 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+// import { Row, Col } from 'react-bootstrap'
+import logo from '../../assets/logoH22.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLocationDot, faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faFacebook, faGithub, faYoutube, faDiscord } from '@fortawesome/free-brands-svg-icons'
+import './Footer.scss'
+
 function Footer() {
     return ( 
-        <h2>Footer</h2>
+        <div className='footer-wrapper'>
+            <div className='footer-row1 row'>
+                <img src={logo} alt='logo' className='footer-logo' />
+            </div>
+            <div className='footer-row2 row'>
+                <div className='footer-tks col-lg-3'>
+                    <h2 className='tks-txt'>Cảm ơn vì đã ghé thăm</h2>
+                </div>
+                <div className='footer-contact col-lg-3'>
+                    <h2 className='contact-title'>Liên hệ</h2>
+                    <div className='location'>
+                        <FontAwesomeIcon icon={faLocationDot}/>
+                        <p className='location-txt'>12 Nguyễn Văn Bảo, Phường 4, Quận Gò Vấp, thành phố Hồ Chí Minh, Việt Nam - Phòng H2.2</p>
+                    </div>
+                    <div className='mail'>
+                        <FontAwesomeIcon icon={faEnvelope}/>
+                        <p className='mail-txt'>aiclub.iuh@gmail.com</p>
+                    </div>
+                </div>
+                <div className='footer-follow col-lg-3'>
+                    <h3 className='follow-title'>Theo dõi chúng tôi</h3>
+                    <div className='follow-wrapper'>
+                        <a href='https://www.facebook.com/aiclub.iuh'className='follow-item'>
+                            <FontAwesomeIcon icon={faFacebook}/>
+                        </a>
+                        <a href='https://www.youtube.com/channel/UCXwozkprKeqF9qstragkIIw' className='follow-item'>
+                            <FontAwesomeIcon icon={faYoutube}/>
+                        </a>
+                        <a href='https://discord.gg/fJJbM8RJ' className='follow-item'>
+                            <FontAwesomeIcon icon={faDiscord}/>
+                        </a>
+                        <a href='https://github.com/AI-CLUB-IUH' className='follow-item'>
+                            <FontAwesomeIcon icon={faGithub}/>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <hr className='line-horizon' />
+            <h6 className='copyright-txt'>© Copyright 2023 – AI CLUB</h6>
+        </div>
      );
 }
 
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,36 +1,39 @@
 import './Header.scss'
-import {Navbar, Container, Nav} from 'react-bootstrap'
+import {Navbar, Nav} from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import logo from '../../assets/logoH22.png'
 
 function Header() {
     return (
         <>
-            <Navbar expand="lg" className="nav header-wrapper">
-                <Container className='header-row'>
+            {/* <Container className='header-wrapper'> */}
+                <Navbar expand="lg" className="nav header-nav">
                     <Navbar.Brand href="/" className='header-left'>
                         <img src={logo} alt='logo' className='logo'/>
                     </Navbar.Brand>
-                    <Navbar.Toggle aria-controls="navbarScroll" color='#fff'/>
+                    <Navbar.Toggle aria-controls="navbarScroll" />
                     <Navbar.Collapse id="navbarScroll" className='header-right'>
-                        <Nav className="me-auto">
+                        <Nav className="me-auto nav-item-wrapper">
                             <Nav.Link href="/" className='nav-item'>Trang chủ</Nav.Link>
                         </Nav>
-                        <Nav className="me-auto">
+                        <Nav className="me-auto nav-item-wrapper">
                             <Nav.Link href="/member" className='nav-item'>Thành viên</Nav.Link>
                         </Nav>
-                        <Nav className="me-auto">
+                        <Nav className="me-auto nav-item-wrapper">
                             <Nav.Link href="/achievement" className='nav-item'>Thành tích</Nav.Link>
                         </Nav>
-                        <Nav className="me-auto">
+                        <Nav className="me-auto nav-item-wrapper">
                             <Nav.Link href="/wiki" className='nav-item'>Wiki</Nav.Link>
                         </Nav>
-                        <Nav className="me-auto">
+                        <Nav className="me-auto nav-item-wrapper">
                             <Nav.Link href="/event" className='nav-item'>Sự kiện</Nav.Link>
                         </Nav>
+                        <Nav className="me-auto nav-item-wrapper login-wrapper">
+                            <Nav.Link href="/log-in" className='btn-login'>Đăng nhập</Nav.Link>
+                        </Nav>
                     </Navbar.Collapse>
-                </Container>
-            </Navbar>
+                </Navbar>
+            {/* </Container> */}
         </>
     )
 }
